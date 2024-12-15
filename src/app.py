aafbcd1a5b1c915a5d3cefd9ef91be844c5b7363
Ricard0-A <ricardoarias4@hotmail.com>
2024-12-15 03:05:12 +0000

    done

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Todos
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,165 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# AHORA MEDIANTE LOS ENDPOINTS VAMOS A AGREGAR EL USUARIO A LA BASE DE DATOS REAL     
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+# Endpoint - Crear Usuario en la base de datos
+@app.route('/users/<string:username>', methods=['POST'])
+def add_new_user(username=None):
+    user = User()
+    # Filtrando si el name la clase User == a username...
+
+    # Forma - 1 
+    user_true = user.query.filter_by(name=username).first()
+    
+    # Si user_true es real ( name si es == username) 
+    if user_true is not None:
+
+        return jsonify({
+            "rejected" : "user already exists" 
+        }), 400
+        
+    # Es decir en cualquier otro caso (Si no es igual, pues crea otro usuario..)
+    else:
+        # Estos 2 codigos para agregar el username al la database
+        user.name = username
+        user.gender = 'MALE'
+        db.session.add(user)
+
+        try:
+            db.session.commit()
+            return jsonify({
+                "id" : user.id,
+                "name" : user.name
+            })
+        except Exception as error:
+            return jsonify(error), 500
+            
+            
+            
+
+# Endpoint - Borrar Usuario en la base de datos
+@app.route('/users/<string:username>', methods=['DELETE'])
+def delete_user(username=None):
+    # Forma - 2 
+    user = User.query.filter_by(name=username).first()
+
+    if user is None:
+        return jsonify({
+            "detail" : f"User {username} doesn't exists"
+        })
+
+    else:
+        try:
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify ([], 204)
+        except Exception as error:
+            return jsonify(error), 500
+    
+                         
+                         
+# Endpoint - Consultar Usuario en la base de datos
+@app.route('/users/<string:username>', methods=['GET'])
+def consultar_user(username=None):
+    user = User.query.filter_by(name=username).one_or_none() #Similar al first() (Pero devuelve solo uno o None)
+
+    if user is None:
+        return jsonify({
+            "detail" : f"user {username} doesn't exists"
+        })
+
+    return jsonify(user.serializable()), 200
+
+
+# Endpoint consultar Todos los Usuarios
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User()
+    # Query.all devuelve todo el contenido de una tabla
+    users = users.query.all()
+    return jsonify({
+        "users" : list(map(lambda unit : unit.serializable_users() , users))
+    })
+
+
+# Endpoint Agregar una tarea to-do 
+@app.route('/todos/<string:username>', methods=['POST'])
+def agregar_todo(username=None):
+    # Recuerda que esto es el objeto que recibo por parte del usuario al hacer POST
+    body = request.json
+ 
+    user = User.query.filter_by(name=username).one_or_none()
+    
+    todos = Todos()
+
+    if body.get("label") is None:
+        return jsonify("Tu body debe conteneder un label"), 400
+    
+    if body.get("is_done") is None:
+        return jsonify("Tu body debe conteneder un is_done"), 400
+ 
+    todos.label = body['label'] 
+    todos.is_done = body.get('is_done')
+    todos.user_id = user.id
+    db.session.add(todos)
+
+    try:
+        db.session.commit()
+        return jsonify('Tarea guardada exitosamente'), 201
+    except Exception as error:
+        # En caso se guarde muchas tareas pero entre muchas  uno falla, se borra todo y no se guarda nada
+        db.session.rollback()
+        return jsonify(error.args), 500
+
+
+# Endpoint Modificar una tarea
+@app.route('/todos/<int:theid>', methods=['PUT'])
+def modificar_tarea(theid):
+    body = request.json
+     # Opcion 1  
+    # todo_edit = Todos.query.filter_by(id=theid).first() 
+    # Opcion 2
+    todo_edit = Todos.query.get(theid)
+
+    if body.get("label") is None:
+        return jsonify("Tu body debe conteneder un label"), 400
+    
+    if body.get("is_done") is None:
+        return jsonify("Tu body debe conteneder un is_done"), 400
+
+    if todo_edit is None:
+        return jsonify({
+            "message" : f"No existe una tarea con el id {theid}"
+        }), 404
+    else:
+        try:
+            todo_edit.label = body["label"]
+            todo_edit.is_done = body["is_done"]
+
+            db.session.commit()
+            return jsonify(todo_edit.serializable()), 201 
+
+        except Exception as error:
+            return jsonify(error.args), 500 
+
+
+# Endpoint Eliminar una Tarea
+@app.route('/todos/<int:theid>', methods=['DELETE'])
+def borrar_tarea(theid):
+    todo = Todos.query.get(theid)
+
+    if todo is None: 
+        return jsonify({"message" : f"{theid} doesn't exists"}), 404
+    else:
+        try:
+            db.session.delete(todo)
+            db.session.commit()
+            return jsonify([]), 204
+        except Exception as error:
+            return jsonify("Error, intentelo mas tarde")
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,134 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
+# Esta vez usaremos sql Alchemy + Flask 
+# para simplificar la creacion de tablas (bd.xxxxxx en vez de importar Integer, Text, Column ETC)
+# Fijate que cosas se tienen que importar
 db = SQLAlchemy()
 
+# Esto es un num y asi se usa
+# Proposito? Una tabla con datos que van a ser estaticos basicamente
+class Genders(Enum):
+    MALE = 'male'
+    FEMALE = 'female'
+    OTHERS = 'other'
+
 class User(db.Model):
+    # Omitimos el tablename 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(225), nullable=False, unique=True)
+    # Enum
+    gender = db.Column(db.Enum(Genders), nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    todos = db.relationship('Todos', back_populates='user', cascade="all, delete") # Cascade En caso de querer borrar usuario y sus tareas a la vez Usa la Cascade...
 
-    def serialize(self):
+    # Recuerda que jsonify acepta diccionarios, no objetos asi que convierte a objeto con ayuda de los self
+    def serializable(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            # Ordenado desde el Frontend Opcion 1
+            "todos" : sorted(
+                [item.serializable for item in self.todos], key=lambda todo : todo["id"] 
+            )
+
+            # Ordenado desde el Frontend Opcion 2
+            # "todos" : sorted(
+            #     list(map(lambda item : item.serializable), self.todos), key=lambda todo : todo["id"]
+            # )
+
+            # Sin ordenar desde el frotend
+            # "todos": list(map(lambda unit: unit.serializable(), self.todos) )
         }
+
+    def serializable_users(self):
+        return {
+            "id" : self.id,
+            "name" : self.name
+        }
+
+
+
+                          
+class Todos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(99), nullable=True)
+    is_done = db.Column(db.Boolean(), nullable=True)
+
+
+    # Necesario para pasar a json la tabla todos desde user (Relacion uno a muchos) 
+    def serializable(self):
+        return {
+            "id": self.id,
+            "label": self.label,
+            "is_done": self.is_done
+        }
+
+    # Recuerda que solo va a conectar la tarea todos con el user solo si es compatible con este ID !
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='todos')
+    
+
+
+# Comandos para correr el proyecto-
+
+# pipenv shell 
+# pipenv run start 
+
+# Ahora necesitamos este ORM y pasarselo a la base de datos real con los comandos... 
+
+# pipenv shell 
+# pipenv run init 
+# Y me crea la carpeta migrations
+ 
+# Luego corre el comando: pipenv run migrate
+# Hasta aqui prepara las migraciones 
+
+# Aun falta el comando y ahora sigue entrar manualmente a la basedatos y crear la database...
+# Comando: psql -h localhost -U gitpod postgres 
+
+# Luego creas la base de datos: create database todos;
+
+# Ahora si, vuelve a darle: pipenv run migrate y listo ahora 
+# se prepara por segunda vez pero ya con la base de data creada + su nombre 
+
+# Ahora por ultimo para guardar las tablas del ORM en la base de datos: \
+# pipen run upgrade
+
+# Ahora vuelve a entrar: psql -h localhost -U gitpod postgres 
+# y posicionate en la base de datos: \c example
+# Ahora para ver las tablas de esa base de datos: \dt 
+
+# Luego si quieres ver todo el contenido, como los registros: select * from <nombre de basedatos>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Y SE BORRO...
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
